9bf58b34bb35f77c660bf3592c3794c5d83f4890
raphaellndr <raph.landure@gmail.com>
2025-03-04 13:44:09 +0100

    Add error handling and display

--- src/components/ui/sudoku/sudoku-creator.tsx
+++ src/components/ui/sudoku/sudoku-creator.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Box, SimpleGrid, Input, VStack, Button } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
+import { toast, ToastContainer } from "react-toastify";
 
 enum Difficulty {
     Unknown = "UNKNOWN",
@@ -12,6 +13,13 @@ enum Difficulty {
 const SudokuGrid = () => {
     const { data: session, status } = useSession();
     const [sudokuGrid, setSudokuGrid] = useState<number[][]>(Array(9).fill(Array(9).fill(0)));
+
+    const notifyError = (message: string) => toast.error(
+        message,
+        {
+            position: "bottom-right",
+        }
+    );
 
     const handleChange = (rowIndex: number, colIndex: number, value: string) => {
         const newSudokuGrid = sudokuGrid.map((row, rIndex) =>
@@ -26,55 +34,63 @@ const SudokuGrid = () => {
 
     const handleCreateSudoku = async () => {
         if (session) {
-            console.log("Session:", session);
             const data = {
                 title: "New Sudoku", // Default title for now
                 difficulty: Difficulty.Unknown, // Default difficulty for now
                 grid: gridToString(),
             };
-            console.log("Creating new sudoku:", data);
-            const response = await fetch(
-                process.env.NEXT_PUBLIC_BACKEND_URL + "sudoku/sudokus/",
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: "Bearer " + session.accessToken
-                    },
-                    body: JSON.stringify(data),
+            try {
+                const response = await fetch(
+                    process.env.NEXT_PUBLIC_BACKEND_URL + "sudoku/sudokus/",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: "Bearer " + session.accessToken
+                        },
+                        body: JSON.stringify(data),
+                    }
+                )
+                if (response.ok) {
+                    toast.success("Successfully created sudoku!");
+                } else {
+                    const errorData = await response.json();
+                    notifyError("Failed to create sudoku: " + errorData);
                 }
-            )
-            const responseData = await response.json();
-            console.log("Created sudoku:", responseData);
-        } else {
-            return;
+            } catch (e: unknown) {
+                const error = e as Error;
+                notifyError(`An error occurred while creating sudoku: ${error.message}`);
+            }
         }
     }
 
     return (
-        <Box p={5}>
-            <VStack>
-                <h1>Sudoku Creator</h1>
-                <SimpleGrid columns={9}>
-                    {sudokuGrid.map((row, rowIndex) =>
-                        row.map((cell, colIndex) => (
-                            <Input
-                                key={`${rowIndex}-${colIndex}`}
-                                width="35px"
-                                type="text"
-                                inputMode="numeric"
-                                pattern="[1-9]"
-                                maxLength={1}
-                                value={cell !== 0 ? cell : ""}
-                                onChange={(e) => handleChange(rowIndex, colIndex, e.target.value)}
-                                textAlign="center"
-                            />
-                        ))
-                    )}
-                </SimpleGrid>
-                <Button onClick={handleCreateSudoku}>Create sudoku</Button>
-            </VStack>
-        </Box>
+        <>
+            <ToastContainer />
+            <Box p={5}>
+                <VStack>
+                    <h1>Sudoku Creator</h1>
+                    <SimpleGrid columns={9}>
+                        {sudokuGrid.map((row, rowIndex) =>
+                            row.map((cell, colIndex) => (
+                                <Input
+                                    key={`${rowIndex}-${colIndex}`}
+                                    width="35px"
+                                    type="text"
+                                    inputMode="numeric"
+                                    pattern="[1-9]"
+                                    maxLength={1}
+                                    value={cell !== 0 ? cell : ""}
+                                    onChange={(e) => handleChange(rowIndex, colIndex, e.target.value)}
+                                    textAlign="center"
+                                />
+                            ))
+                        )}
+                    </SimpleGrid>
+                    <Button onClick={handleCreateSudoku}>Create sudoku</Button>
+                </VStack>
+            </Box>
+        </>
     );
 };
 
