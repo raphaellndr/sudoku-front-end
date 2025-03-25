03c773446a69ebd3332cc42b68a5530c0dcbda76
raphaellndr <raph.landure@gmail.com>
2025-03-25 11:14:48 +0100

    Add SudokuSolutionSchema and improve SudokuSchema

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -19,20 +19,50 @@ export const UserFormSchema = z.object({
 }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
-})
+});
+
+export const SudokuSolutionSchema = z.object({
+    id: z
+        .string()
+        .uuid({ message: "Must be a valid UUID" }),
+    sudoku_id: z
+        .string()
+        .uuid({ message: "Must be a valid UUID" }),
+    grid: z
+        .string()
+        .length(81, { message: "Solution grid must contain 81 characters" })
+        .nullable()
+        .optional(),
+    created_at: z
+        .string(),
+    updated_at: z
+        .string(),
+});
 
 export const SudokuSchema = z.object({
     id: z
-        .string(),
+        .string()
+        .uuid({ message: "Must be a valid UUID" }),
+    user_id: z
+       .string()
+       .uuid({ message: "Must be a valid UUID" }),
     title: z
-        .string(),
+        .string()
+        .max(255),
     difficulty: z
         .enum(SudokuDifficultyEnum.options),
     grid: z
         .string()
         .length(81, { message: "Sudoku must contain 81 characters" }),
+    task_id: z
+       .string()
+       .max(255)
+       .nullable()
+       .optional(),
+    solution: SudokuSolutionSchema
+        .optional(),
     created_at: z
         .string(),
     updated_at: z
         .string(),
-})
+});
