8603884f475cc3ca09d51df940030b763abed1ad
raphaellndr <raph.landure@gmail.com>
2025-03-25 11:13:56 +0100

    Run and abort solving task

--- src/components/ui/sudoku/sudoku-list.tsx
+++ src/components/ui/sudoku/sudoku-list.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 
-import { Box, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 
 import { Sudoku } from "@/types/types";
+import { useSession } from "next-auth/react";
+import { notifyError, notifySuccess } from "@/toasts/toast";
 
 interface SudokuGridProps {
     sudokuGrid: string;
@@ -40,9 +42,63 @@ interface SudokuListProps {
 };
 
 const SudokuList: React.FC<SudokuListProps> = ({ sudokus, onFetchSudokus }) => {
+    const { data: session, status } = useSession();
+
     useEffect(() => {
         onFetchSudokus();
-      }, []);
+    }, []);
+
+    const handleAbortButton = async (sudoku: Sudoku) => {
+        if (session) {
+            try {
+                const response = await fetch(
+                    process.env.NEXT_PUBLIC_BACKEND_URL + `sudoku/sudokus/${sudoku.id}/solution/`,
+                    {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: "Bearer " + session.accessToken
+                        },
+                    }
+                )
+                const responseData = await response.json()
+                if (response.ok) {
+                    notifySuccess("Task aborted successfully!")
+                } else {
+                    notifyError("Failed to abort task: " + responseData);
+                }
+            } catch (e: unknown) {
+                const error = e as Error;
+                notifyError(`An error occurred while aborting the task: ${error.message}`);
+            }
+        }
+    }
+
+    const handleSolveButton = async (sudoku: Sudoku) => {
+        if (session) {
+            try {
+                const response = await fetch(
+                    process.env.NEXT_PUBLIC_BACKEND_URL + `sudoku/sudokus/${sudoku.id}/solution/`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: "Bearer " + session.accessToken
+                        },
+                    }
+                )
+                const responseData = await response.json()
+                if (response.ok) {
+                    notifySuccess("Task run successfully!")
+                } else {
+                    notifyError("Failed to run task: " + responseData);
+                }
+            } catch (e: unknown) {
+                const error = e as Error;
+                notifyError(`An error occurred while running task: ${error.message}`);
+            }
+        }
+    };
 
     return (
         <>
@@ -51,6 +107,8 @@ const SudokuList: React.FC<SudokuListProps> = ({ sudokus, onFetchSudokus }) => {
                     <Box key={index}>
                         <Text mb={2}>Sudoku {sudoku.id} - {sudoku.difficulty}</Text>
                         <DisplaySudokuGrid sudokuGrid={sudoku.grid} />
+                        <Button onClick={() => handleAbortButton(sudoku)}>Abort solving</Button>
+                        <Button onClick={() => handleSolveButton(sudoku)}>Solve sudoku</Button>
                     </Box>
                 ))}
             </VStack>
