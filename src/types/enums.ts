510aa12861887e8a376717ef92fe3247be82ff06
raphaellndr <raph.landure@gmail.com>
2025-03-25 14:30:13 +0100

    Set enums to lowercase to comply with backend and set correct values for SudokuStatusEnum

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -2,5 +2,5 @@
 
 import { z } from "zod";
 
-export const SudokuDifficultyEnum = z.enum(["Unknown", "Easy", "Medium", "Hard"]);
-export const SudokuStatusEnum = z.enum(["Unknown", "Easy", "Medium", "Hard"]);
+export const SudokuDifficultyEnum = z.enum(["unknown", "easy", "medium", "hard"]);
+export const SudokuStatusEnum = z.enum(["created", "running", "pending", "completed", "failed", "aborted", "invalid"]);
