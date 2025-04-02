8ffa34b3e95a37270480c903175d749c2577c26e
raphaellndr <raph.landure@gmail.com>
2025-04-02 09:13:42 +0200

    Wrap app with QueryClientProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,25 +4,30 @@ import { Inter } from "next/font/google"
 import { SessionProvider } from "next-auth/react"
 
 import Provider from "@/components/ui/provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+    subsets: ["latin"],
+    display: "swap",
 })
 
+const queryClient = new QueryClient()
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html className={inter.className} suppressHydrationWarning>
-      <head />
-      <body>
-        <SessionProvider>
-          <Provider>{children}</Provider>
-        </SessionProvider>
-      </body>
-    </html>
-  )
+    return (
+        <html className={inter.className} suppressHydrationWarning>
+            <head />
+            <body>
+                <QueryClientProvider client={queryClient}>
+                    <SessionProvider>
+                        <Provider>{children}</Provider>
+                    </SessionProvider>
+                </QueryClientProvider>
+            </body>
+        </html>
+    )
 }
