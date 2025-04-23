8b93f2d5f8302e4bff40c45176df4199144f6c57
raphaellndr <raph.landure@gmail.com>
2025-04-23 15:53:49 +0200

    Add SignInFormValues and rename UserFormValues to RegisterFormValues

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,11 +3,12 @@
 import { z } from "zod";
 
 import { SudokuDifficultyEnum, SudokuStatusEnum } from "./enums";
-import { SudokuSchema, SudokuSolutionSchema, UserFormSchema } from "./schemas";
+import { RegisterFormSchema, SignInFormSchema, SudokuSchema, SudokuSolutionSchema } from "./schemas";
 
 export type Sudoku = z.infer<typeof SudokuSchema>
 export type SudokuSolution = z.infer<typeof SudokuSolutionSchema>
 export type SudokuDifficulty = z.infer<typeof SudokuDifficultyEnum>;
 export type SudokuStatus = z.infer<typeof SudokuStatusEnum>;
 
-export type UserFormValues = z.infer<typeof UserFormSchema>;
+export type RegisterFormValues = z.infer<typeof RegisterFormSchema>;
+export type SignInFormValues = z.infer<typeof SignInFormSchema>;
