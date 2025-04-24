9c5b354fc21f30aa214d63a6f091bcc6b52ca12e
raphaellndr <raph.landure@gmail.com>
2025-04-24 14:47:39 +0200

    Add SudokuStatus (Floating i ndicator)

---
+++ src/components/ui/sudoku/sudoku-status.tsx
@@ -0,0 +1,27 @@
+import { Float, Status } from "@chakra-ui/react";
+
+import { SudokuStatus as SudokuStatusType } from "@/types/types";
+
+const getStatusColor = (status: string | null) => {
+    switch (status) {
+        case "completed": return "green";
+        case "running": return "blue";
+        case "pending": return "yellow";
+        case "failed": return "red";
+        case "invalid": return "red";
+        case "aborted": return "orange";
+        default: return "gray";
+    }
+};
+
+const SudokuStatus = ({ status }: { status: SudokuStatusType | null }) => {
+    return (
+        <Float>
+            <Status.Root colorPalette={getStatusColor(status)}>
+                <Status.Indicator />
+            </Status.Root>
+        </Float>
+    );
+};
+
+export default SudokuStatus;
