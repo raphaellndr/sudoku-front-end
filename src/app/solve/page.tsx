7d4f1e35b9a4cc2be43d23033d95a458a2624e3e
raphaellndr <raph.landure@gmail.com>
2025-04-24 14:47:24 +0200

    Add /solve page

---
+++ src/app/solve/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+import { Box, Flex, Separator, Show, Spinner } from "@chakra-ui/react";
+import { ToastContainer } from "react-toastify";
+
+import Header from "@/components/ui/home-page/header/header";
+import Footer from "@/components/ui/home-page/footer/footer";
+import SudokuSolver from "@/components/ui/sudoku/sudoku-solver";
+
+export default function Home() {
+    const { status } = useSession();
+
+    return (
+        <Flex direction="column" minHeight="100vh">
+            <ToastContainer />
+            <Header />
+            <Box flex="1">
+                <Show
+                    when={status !== "loading"}
+                    fallback={<Spinner />}
+                >
+                    <SudokuSolver />
+                </Show>
+            </Box>
+            <Separator marginLeft="5" marginRight="5" />
+            <Footer />
+        </Flex>
+    );
+};
