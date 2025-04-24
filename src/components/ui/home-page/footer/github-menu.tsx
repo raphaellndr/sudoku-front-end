f3f570745f902960fbef4fa07b8dc006258ef251
raphaellndr <raph.landure@gmail.com>
2025-04-24 09:05:12 +0200

    Add menu to redirect to source code

---
+++ src/components/ui/home-page/footer/github-menu.tsx
@@ -0,0 +1,56 @@
+import { IconButton, Menu, Portal, Text } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+
+const GithubMenu = () => {
+    return (
+        <Menu.Root>
+            <Menu.Trigger asChild>
+                <IconButton
+                    asChild
+                    variant="ghost"
+                    _hover={{ backgroundColor: "transparent" }}
+                    _expanded={{ backgroundColor: "transparent" }}
+                >
+                    <div>
+                        <FaGithub />
+                        <Text display={{ base: "none", md: "flex" }}>Source code</Text>
+                    </div>
+                </IconButton>
+            </Menu.Trigger>
+            <Portal>
+                <Menu.Positioner>
+                    <Menu.Content>
+                        {repoLinks.map((link) => (
+                            <Menu.Item key={link.value} asChild value={link.value}>
+                                <a href={link.href} target="_blank" rel="noreferrer">
+                                    {link.label}
+                                </a>
+                            </Menu.Item>
+                        ))}
+                    </Menu.Content>
+                </Menu.Positioner>
+            </Portal>
+        </Menu.Root>
+    );
+};
+
+const repoLinks = [
+    {
+        value: "frontend",
+        href: "https://github.com/raphaellndr/sudoku-front-end",
+        label: "Frontend",
+    },
+    {
+        value: "backend",
+        href: "https://github.com/raphaellndr/SudokuAPI",
+        label: "Backend",
+    },
+    {
+        value: "resolution",
+        href: "https://github.com/raphaellndr/sudoku-resolver",
+        label: "Computation logic",
+    },
+
+]
+
+export default GithubMenu;
