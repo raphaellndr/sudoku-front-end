538208d5ebe2d9a8bbca0d504f23712b4c7df1b6
raphaellndr <raph.landure@gmail.com>
2025-04-24 09:06:20 +0200

    Improve footer

--- src/components/ui/home-page/footer/footer.tsx
+++ src/components/ui/home-page/footer/footer.tsx
@@ -1,28 +1,23 @@
 "use client"
 
-import { Box, Container, Stack, Text, Link, HStack, Icon } from "@chakra-ui/react";
-import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { Box, Container, Text, HStack } from "@chakra-ui/react";
+
+import EmailButton from "./email-button";
+import LinkedInButton from "./linkedin-button";
+import GithubMenu from "./github-menu";
 
 const Footer = () => {
     return (
         <Box py={8} borderTop="1px" borderColor="gray.200" mt="auto">
             <Container maxW="container.lg">
-                <Stack direction={["column", "row"]} justify="space-between" align="center">
+                <HStack justify="space-between" align="center">
                     <Text>&copy; {new Date().getFullYear()} SudokuSolver. All rights reserved.</Text>
-
-                    <Stack direction="row" gap={6}>
-                        <Link href="https://github.com/raphaellndr/your-repo">
-                            <HStack>
-                                <Icon as={FaGithub} boxSize={5} />
-                                <Text>Source Code</Text>
-                            </HStack>
-                        </Link>
-
-                        <Link href="https://www.linkedin.com/in/rapha%C3%ABl-landur%C3%A9-16028a183/">
-                            <Icon as={FaLinkedin} boxSize={5} />
-                        </Link>
-                    </Stack>
-                </Stack>
+                    <HStack gap={6}>
+                        <GithubMenu />
+                        <LinkedInButton />
+                        <EmailButton />
+                    </HStack>
+                </HStack>
             </Container>
         </Box>
     );
