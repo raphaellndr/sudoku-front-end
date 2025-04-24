e623c2450c9c41407a58da62d10ef1ef693c1c86
raphaellndr <raph.landure@gmail.com>
2025-04-24 09:05:34 +0200

    Add button to redirect to my linkedin profile

---
+++ src/components/ui/home-page/footer/linkedin-button.tsx
@@ -0,0 +1,18 @@
+import { IconButton, Text } from "@chakra-ui/react";
+import { FaLinkedin } from "react-icons/fa";
+
+const LinkedInButton = () => {
+    return (
+        <IconButton asChild variant="ghost"
+            _hover={{
+                backgroundColor: "transparent",
+            }}>
+            <a href="https://www.linkedin.com/in/rapha%C3%ABl-landur%C3%A9-16028a183/">
+                <FaLinkedin />
+                <Text display={{ base: "none", md: "flex" }}>LinkedIn profile</Text>
+            </a>
+        </IconButton>
+    );
+};
+
+export default LinkedInButton;
