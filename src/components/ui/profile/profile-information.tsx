d31fd2d74eea6e1f5399b2c304ce7c5fcf8cfd86
raphaellndr <raph.landure@gmail.com>
2025-04-25 12:35:29 +0200

    Add ProfileInformation congtaining information about the current user

---
+++ src/components/ui/profile/profile-information.tsx
@@ -0,0 +1,22 @@
+import { useSession } from "next-auth/react";
+
+import { Avatar, HStack, Text, VStack } from "@chakra-ui/react";
+
+
+const ProfileInformation = () => {
+    const { data: session } = useSession();
+
+    return (
+        <HStack gap={4}>
+            <Avatar.Root size="xl">
+                <Avatar.Fallback name={session?.user?.username} />
+            </Avatar.Root>
+            <VStack align="start">
+                <Text fontSize="2xl" fontWeight="bold">{session?.user?.username}</Text>
+                <Text fontSize="md" color="gray.500">{session?.user?.email}</Text>
+            </VStack>
+        </HStack>
+    );
+};
+
+export default ProfileInformation;
