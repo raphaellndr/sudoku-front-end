534d102aab79a58d926befa53b00db543ff70be6
raphaellndr <raph.landure@gmail.com>
2025-04-25 12:37:03 +0200

    Modify /profile to display stats (fake ones) and the ability to change user information (WIP)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,70 +1,24 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { Box, Flex, VStack, Separator } from "@chakra-ui/react";
 
-import { signOut, useSession } from "next-auth/react";
-import { Box, Button, Code, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
+import Header from "@/components/ui/home-page/header/header";
+import Footer from "@/components/ui/home-page/footer/footer";
+import ProfileAccordion from "@/components/ui/profile/profile-accordion";
+import ProfileInformation from "@/components/ui/profile/profile-information";
 
 export default function Home() {
-    const { data: session, status } = useSession({ required: true });
-    const [response, setResponse] = useState("{}");
-    const router = useRouter()
-
-    const getUserDetails = async (useToken: boolean) => {
-        try {
-            const response = await fetch(
-                process.env.NEXT_PUBLIC_BACKEND_URL + "api/auth/user/",
-                {
-                    headers: useToken ? { Authorization: "Bearer " + session?.accessToken } : {},
-                }
-            );
-            const responseData = await response.json();
-            if (response.ok) {
-                setResponse(JSON.stringify(responseData));
-            } else {
-                setResponse("An error occurred: " + `${response.status} ${response.statusText}`);
-            }
-        } catch (error) {
-            if (error instanceof Error) {
-                setResponse(error.message);
-            } else {
-                setResponse("An unknown error occurred.");
-            }
-        }
-    };
-
-    if (status == "loading") {
-        return <Spinner size="lg"></Spinner>
-    }
-
-    if (session) {
-        return (
-            <Box m={8}>
-                <VStack>
-                    <Text>Username: {session.user.username}</Text>
-                    <Text>Email: {session.user.email || "Not provided"}</Text>
-                    <Code>
-                        {response}
-                    </Code>
+    return (
+        <Flex direction="column" minHeight="100vh">
+            <Header />
+            <Box flex="1" maxW={"100"}>
+                <VStack margin="10">
+                    <ProfileInformation />
+                    <ProfileAccordion />
                 </VStack>
-                <HStack justifyContent="center" mt={4}>
-                    <Button colorScheme="blue" onClick={() => getUserDetails(true)}>
-                        User details (with token)
-                    </Button>
-                    <Button colorScheme="orange" onClick={() => getUserDetails(false)}>
-                        User details (without token)
-                    </Button>
-                    <Button colorScheme="red" onClick={() => router.push("/")}>
-                        Back to home page
-                    </Button>
-                    <Button colorScheme="red" onClick={() => signOut({ callbackUrl: "/" })}>
-                        Sign out
-                    </Button>
-                </HStack>
             </Box>
-        );
-    }
-
-    return <>Unauthenticated</>;
+            <Separator marginLeft="5" marginRight="5" />
+            <Footer />
+        </Flex>
+    );
 }
