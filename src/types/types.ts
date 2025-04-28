03a29d6b549725622ae7525df802e54caab27b18
raphaellndr <raph.landure@gmail.com>
2025-04-28 17:07:36 +0200

    Add SettingsFormValues

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,7 +3,7 @@
 import { z } from "zod";
 
 import { SudokuDifficultyEnum, SudokuStatusEnum } from "./enums";
-import { RegisterFormSchema, SignInFormSchema, SudokuSchema, SudokuSolutionSchema } from "./schemas";
+import { RegisterFormSchema, SettingsFormSchema, SignInFormSchema, SudokuSchema, SudokuSolutionSchema } from "./schemas";
 
 export type Sudoku = z.infer<typeof SudokuSchema>
 export type SudokuSolution = z.infer<typeof SudokuSolutionSchema>
@@ -12,3 +12,5 @@ export type SudokuStatus = z.infer<typeof SudokuStatusEnum>;
 
 export type RegisterFormValues = z.infer<typeof RegisterFormSchema>;
 export type SignInFormValues = z.infer<typeof SignInFormSchema>;
+
+export type SettingsFormValues = z.infer<typeof SettingsFormSchema>;
