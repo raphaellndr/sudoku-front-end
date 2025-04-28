2f3aaad74b0f73d2daf3750ae7fb0c70d44deda4
raphaellndr <raph.landure@gmail.com>
2025-04-28 17:07:51 +0200

    Add ToastContainer

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Box, Flex, VStack, Separator } from "@chakra-ui/react";
+import { ToastContainer } from "react-toastify";
 
 import Header from "@/components/ui/home-page/header/header";
 import Footer from "@/components/ui/home-page/footer/footer";
@@ -10,6 +11,7 @@ import ProfileInformation from "@/components/ui/profile/profile-information";
 export default function Home() {
     return (
         <Flex direction="column" minHeight="100vh">
+            <ToastContainer />
             <Header />
             <Box flex="1" maxW={"100"}>
                 <VStack margin="10">
