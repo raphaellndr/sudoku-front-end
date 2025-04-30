4b2ecdc3a0b0140bfdc08992c5ac5341a5b95024
raphaellndr <raph.landure@gmail.com>
2025-04-30 14:55:32 +0200

    Add UndoButton

---
+++ src/components/ui/sudoku/play-mode/undo-button.tsx
@@ -0,0 +1,44 @@
+import { Box, Circle, Float, IconButton } from "@chakra-ui/react";
+import { CiUndo } from "react-icons/ci";
+
+import { useColorModeValue } from "../../color-mode";
+
+export const MAX_UNDOS = 3;
+
+interface UndoButtonprops {
+    canUndo: boolean;
+    undoMove: () => void;
+    remainingUndos: number;
+}
+
+export const UndoButton: React.FC<UndoButtonprops> = (
+    { canUndo, undoMove, remainingUndos }
+) => {
+    const circleBg = useColorModeValue("black", "white");
+    const circleBgDisabled = useColorModeValue("gray", "gray");
+    const circleColor = useColorModeValue("white", "black");
+
+    const isDisabled = !canUndo || remainingUndos === 0
+
+    return (
+        <Box position="relative">
+            <IconButton
+                disabled={isDisabled}
+                variant="outline"
+                width="100%"
+                onClick={undoMove}
+            >
+                <CiUndo />
+                Undo
+            </IconButton>
+            <Float>
+                <Circle
+                    size="4"
+                    bg={isDisabled? circleBgDisabled : circleBg}
+                    color={circleColor}>
+                    {remainingUndos}
+                </Circle>
+            </Float>
+        </Box>
+    );
+};
