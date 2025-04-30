d9db8ab341856961cc28322301526b846908389c
raphaellndr <raph.landure@gmail.com>
2025-04-30 14:15:20 +0200

    Create useTimer hook

---
+++ src/components/ui/sudoku/play-mode/timer/use-timer.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+
+/**
+ * Custom hook to manage the timer state during play mode
+ */
+export const useTimer = () => {
+    const [timer, setTimer] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
+
+    const timerRef = useRef<NodeJS.Timeout | null>(null);
+
+    useEffect(() => {
+        if (isActive && !isPaused) {
+            timerRef.current = setInterval(() => {
+                setTimer(prev => prev + 1);
+            }, 1000);
+        } else if (timerRef.current) {
+            clearInterval(timerRef.current);
+        }
+
+        return () => {
+            if (timerRef.current) {
+                clearInterval(timerRef.current);
+            }
+        };
+    }, [isActive, isPaused]);
+
+    return {
+        timer,
+        setTimer,
+        isActive,
+        setIsActive,
+        isPaused,
+        setIsPaused,
+        timerRef,
+    };
+};
