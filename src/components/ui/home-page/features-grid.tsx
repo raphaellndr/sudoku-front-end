5441218b6a12e147edd8bd1bbfd8a3e14bb86970
raphaellndr <raph.landure@gmail.com>
2025-05-20 15:20:08 +0200

    Use CustomCard

--- src/components/ui/home-page/features-grid.tsx
+++ src/components/ui/home-page/features-grid.tsx
@@ -1,20 +1,20 @@
 import { Container, SimpleGrid } from "@chakra-ui/react";
 
-import { Feature } from "./feature";
+import CustomCard from "./custom-card";
 
 const FeaturesGrid = () => {
     return (
         <Container maxW="container.lg" py={10}>
             <SimpleGrid columns={{ base: 1, md: 3 }} gap={10}>
-                <Feature
+                <CustomCard
                     title="Play Sudokus"
                     description="Create your own Sudoku puzzle and play with it! Compete against time, get hints when stuck, and check your results."
                 />
-                <Feature
+                <CustomCard
                     title="Solve Sudokus"
                     description="Fill in a grid and solve it using various algorithms. View solution and solving time."
                 />
-                <Feature
+                <CustomCard
                     title="User Stats"
                     description="Create an account to track your progress and achievements."
                 />
