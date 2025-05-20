f91f9e667f8991e174b2e244528274f0ed0d00dc
raphaellndr <raph.landure@gmail.com>
2025-05-20 12:27:23 +0200

    Add SudokuArenaButton

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,17 +1,33 @@
 "use client"
 
-import { Center, Heading, Stack, VStack } from "@chakra-ui/react";
+import { Center, Heading, Stack, Text, VStack } from "@chakra-ui/react";
 
 import RegistrationForm from "@/components/ui/account/registration/registration-form";
 import OrSeparator from "@/components/ui/or-separator";
 import GoogleButton from "@/components/ui/account/google-button";
 import SignInPrompt from "@/components/ui/account/registration/sign-in-prompt";
+import { SudokuArenaButton } from "@/components/ui/sudoku-arena-button";
 
 export default function Home() {
     return (
         <Center height="100vh">
             <VStack gap="10">
-                <Heading size="6xl" textAlign="center">Create your account</Heading>
+                <VStack>
+                    <Text
+                        textStyle="6xl"
+                        textAlign="center"
+                    >
+                        New to <SudokuArenaButton
+                            textProps={{ fontSize: "6xl" }}
+                            buttonProps={{
+                                alignItems: "baseline",
+                                p: "0",
+                                m: "0"
+                            }}
+                        />?
+                    </Text>
+                    <Heading size="6xl" textAlign="center">Create your account</Heading>
+                </VStack>
                 <Stack gap="4">
                     <RegistrationForm />
                     <OrSeparator />
