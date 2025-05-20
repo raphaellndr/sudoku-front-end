51e7d4553d60abee149c747b34e16c00dca31e07
raphaellndr <raph.landure@gmail.com>
2025-05-20 15:52:16 +0200

    Add About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Box, Container, Flex, Separator, VStack } from "@chakra-ui/react";
+
+import Header from "@/components/ui/header/header";
+import Footer from "@/components/ui/footer/footer";
+import AboutHeader from "@/components/ui/about-page/about-header";
+import AboutSections from "@/components/ui/about-page/about-sections";
+
+export default function Home() {
+    return (
+        <Flex direction="column" minHeight="100vh">
+            <Header />
+            <Box flex="1" maxW={"100"}>
+                <Container maxW="container.lg">
+                    <VStack gap={8} align="stretch">
+                        <AboutHeader />
+                        <AboutSections />
+                    </VStack>
+                </Container>
+            </Box>
+            <Separator marginLeft="5" marginRight="5" />
+            <Footer />
+        </Flex>
+    );
+};
