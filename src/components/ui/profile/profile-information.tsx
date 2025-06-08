50176c59168d651022198263ea3335a0f4ac6881
raphaellndr <raph.landure@gmail.com>
2025-06-08 19:06:40 +0200

    Show spinner if no session or user

--- src/components/ui/profile/profile-information.tsx
+++ src/components/ui/profile/profile-information.tsx
@@ -1,13 +1,21 @@
 import { useSession } from "next-auth/react";
-import { Avatar, HStack, Text, VStack } from "@chakra-ui/react";
+import { Avatar, Flex, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
 
 const ProfileInformation = () => {
     const { data: session } = useSession();
 
+    if (!session || !session.user) {
+        return (
+            <Flex justify="center" align="center" height="300px">
+                <Spinner size="xl" color="blue.500" />
+            </Flex>
+        );
+    }
+
     return (
         <HStack gap={4}>
             <Avatar.Root size="xl">
-                <Avatar.Fallback name={session?.user?.username} />
+                <Avatar.Fallback name={session.user.username ? session.user.username : ""} />
             </Avatar.Root>
             <VStack align="start">
                 <Text fontSize="2xl" fontWeight="bold">{session?.user?.username}</Text>
