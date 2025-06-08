8b1c9c6a28440f3f27ab42f4247d9e5db22d9de0
raphaellndr <raph.landure@gmail.com>
2025-06-08 19:03:48 +0200

    Reformat and improve Session interface

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,17 +4,21 @@ import NextAuth from "next-auth";
 NextAuth.name;
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context.
-   * Session `user` should only contain immutable variables.
-   */
-  interface Session {
-    accessToken: string;
-    refreshToken: string;
-    user: {
-      username: string;
-      email: string | null;
-      date_joined: string;
-    };
-  }
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context.
+     * Session `user` should only contain immutable variables.
+     */
+    interface Session {
+        accessToken: string;
+        refreshToken: string;
+        user: {
+            id: string;
+            username: string | null;
+            email: string;
+            is_active: boolean;
+            is_staff: boolean;
+            created_at: string;
+            updated_at: string;
+        };
+    }
 }
