da1354bb1f7ff051f68e3e73ffff15c07ac1399e
raphaellndr <raph.landure@gmail.com>
2025-06-08 19:02:25 +0200

    Add GameRecord, GameStatus and UserStats types

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,8 +2,16 @@
 
 import { z } from "zod";
 
-import { SudokuDifficultyEnum, SudokuStatusEnum } from "./enums";
-import { RegisterFormSchema, SettingsFormSchema, SignInFormSchema, SudokuSchema, SudokuSolutionSchema } from "./schemas";
+import { GameStatusEnum, SudokuDifficultyEnum, SudokuStatusEnum } from "./enums";
+import {
+    GameRecordSchema,
+    RegisterFormSchema,
+    SettingsFormSchema,
+    SignInFormSchema,
+    SudokuSchema,
+    SudokuSolutionSchema,
+    UserStatsSchema
+} from "./schemas";
 
 export type Sudoku = z.infer<typeof SudokuSchema>
 export type SudokuSolution = z.infer<typeof SudokuSolutionSchema>
@@ -14,3 +22,8 @@ export type RegisterFormValues = z.infer<typeof RegisterFormSchema>;
 export type SignInFormValues = z.infer<typeof SignInFormSchema>;
 
 export type SettingsFormValues = z.infer<typeof SettingsFormSchema>;
+
+export type GameRecord = z.infer<typeof GameRecordSchema>;
+export type GameStatus = z.infer<typeof GameStatusEnum>;
+
+export type UserStats = z.infer<typeof UserStatsSchema>;
