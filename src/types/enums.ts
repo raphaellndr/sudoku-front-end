44eeadd3db75bfcf7d21bf8ff1b3106a060fcd77
raphaellndr <raph.landure@gmail.com>
2025-06-08 19:02:25 +0200

    Add GameStatusEnum

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -4,3 +4,5 @@ import { z } from "zod";
 
 export const SudokuDifficultyEnum = z.enum(["unknown", "easy", "medium", "hard"]);
 export const SudokuStatusEnum = z.enum(["created", "running", "pending", "completed", "failed", "aborted", "invalid"]);
+
+export const GameStatusEnum = z.enum(["in_progress", "completed", "abandoned", "stopped"]);
