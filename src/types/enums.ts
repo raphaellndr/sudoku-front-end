e2f04e0ad7aa267e7cd80c46828b94951c5ee604
raphaellndr <raph.landure@gmail.com>
2025-06-11 11:40:26 +0200

    Add StatsPeriodEnum

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -6,3 +6,5 @@ export const SudokuDifficultyEnum = z.enum(["unknown", "easy", "medium", "hard"]
 export const SudokuStatusEnum = z.enum(["created", "running", "pending", "completed", "failed", "aborted", "invalid"]);
 
 export const GameStatusEnum = z.enum(["in_progress", "completed", "abandoned", "stopped"]);
+
+export const StatsPeriodEnum = z.enum(["daily", "weekly", "monthly", "yearly", "allTime"]);
