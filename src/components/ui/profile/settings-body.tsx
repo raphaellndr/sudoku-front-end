dc688ce031ae58df0aa7a42e90b7d091628d2ce7
raphaellndr <raph.landure@gmail.com>
2025-06-12 12:19:11 +0200

    Only reroute the user when they aren't authenticated

--- src/components/ui/profile/settings-body.tsx
+++ src/components/ui/profile/settings-body.tsx
@@ -3,12 +3,12 @@ import { useRouter } from "next/navigation";
 
 import { useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
-import { Accordion, Button, Field, Input, Stack } from "@chakra-ui/react";
+import { Accordion, Button, Field, Flex, Input, Spinner, Stack } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { SettingsFormValues } from "@/types/types";
 import { SettingsFormSchema } from "@/types/schemas";
-import { notifyError, notifySuccess } from "@/toasts/toast";
+import { notifySuccess } from "@/toasts/toast";
 import { createHeaders } from "@/utils/apiUtils";
 import { partialUpdateCurrentUser } from "@/services/meApi";
 
@@ -39,10 +39,10 @@ const SettingsBody = () => {
     });
 
     useEffect(() => {
-        if (!session) {
+        if (status === "unauthenticated") {
             router.push("/solver")
         }
-    }, [session])
+    }, [status])
 
     const emailValue = watch("email");
 
