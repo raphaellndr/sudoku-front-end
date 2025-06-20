a61269e54a7fbed8a05f3daf07fa772e00a61e75
raphaellndr <raph.landure@gmail.com>
2025-06-20 21:46:59 +0200

    Add GamesHistory component (WIP)

---
+++ src/components/ui/profile/games-history/games-history-body.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+const GamesHistoryBody = () => {
+    const fetchGamesHistory = async () => {
+        
+    };
+
+    useEffect(() => {
+
+    }), []
+    return (
+        <></>
+    );
+};
+
+export default GamesHistoryBody;
