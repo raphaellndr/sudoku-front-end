06ece90ddc57dad640465cd0a09510355dc5da07
raphaellndr <raph.landure@gmail.com>
2025-06-20 21:47:32 +0200

    Use AccordionItem and add GamesHistoryBody

--- src/components/ui/profile/profile-accordion.tsx
+++ src/components/ui/profile/profile-accordion.tsx
@@ -1,50 +1,45 @@
-import { Accordion, Icon } from "@chakra-ui/react";
+import { Accordion } from "@chakra-ui/react";
 import { ImStatsDots } from "react-icons/im";
 import { LuSettings } from "react-icons/lu";
 import { IoMdStats } from "react-icons/io";
+import { FaHistory } from "react-icons/fa";
 
 import SettingsBody from "./settings-body";
 import ChartsBody from "./charts/charts-body";
 import StatsBody from "./stats/stats-body";
+import GamesHistoryBody from "./games-history/games-history-body";
+import AccordionItem from "./accordion-item";
 
 const ProfileAccordion = () => {
     return (
-        <Accordion.Root multiple defaultValue={["stats"]}>
-            <Accordion.Item value="stats">
-                <Accordion.ItemTrigger>
-                    <Icon fontSize="lg" color="fg.subtle">
-                        <IoMdStats />
-                    </Icon>
-                    Stats
-                </Accordion.ItemTrigger>
-                <Accordion.ItemContent>
-                    <StatsBody />
-                </Accordion.ItemContent>
-            </Accordion.Item>
+        <Accordion.Root multiple collapsible defaultValue={["history"]}>
+            <AccordionItem
+                value="history"
+                title="Games History"
+                icon={<FaHistory />}
+                content={<GamesHistoryBody />}
+            />
 
-            <Accordion.Item value="charts">
-                <Accordion.ItemTrigger>
-                    <Icon fontSize="lg" color="fg.subtle">
-                        <ImStatsDots />
-                    </Icon>
-                    Charts
-                </Accordion.ItemTrigger>
-                <Accordion.ItemContent>
-                    <ChartsBody />
-                </Accordion.ItemContent>
-            </Accordion.Item>
+            <AccordionItem
+                value="stats"
+                title="Stats"
+                icon={<ImStatsDots />}
+                content={<StatsBody />}
+            />
 
-            <Accordion.Item value="settings">
-                <Accordion.ItemTrigger>
-                    <Icon fontSize="lg" color="fg.subtle">
-                        <LuSettings />
-                    </Icon>
-                    Settings
-                </Accordion.ItemTrigger>
-                <Accordion.ItemContent>
-                    <SettingsBody />
-                </Accordion.ItemContent>
-            </Accordion.Item>
+            <AccordionItem
+                value="charts"
+                title="Charts"
+                icon={<IoMdStats />}
+                content={<ChartsBody />}
+            />
+
+            <AccordionItem
+                value="settings"
+                title="Settings"
+                icon={<LuSettings />}
+                content={<SettingsBody />}
+            />
         </Accordion.Root>
     );
 };
