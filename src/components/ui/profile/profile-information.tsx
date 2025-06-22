da3c6507284bc1d57f0f36cb857b77d1de484128
raphaellndr <raph.landure@gmail.com>
2025-06-22 18:13:53 +0200

    Wrap HStack inside a Card

--- src/components/ui/profile/profile-information.tsx
+++ src/components/ui/profile/profile-information.tsx
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import { Avatar, Flex, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
+import { Avatar, Card, Flex, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
 
 const ProfileInformation = () => {
     const { data: session } = useSession();
@@ -13,15 +13,19 @@ const ProfileInformation = () => {
     }
 
     return (
-        <HStack gap={4}>
-            <Avatar.Root size="xl">
-                <Avatar.Fallback name={session.user.username ? session.user.username : ""} />
-            </Avatar.Root>
-            <VStack align="start">
-                <Text fontSize="2xl" fontWeight="bold">{session?.user?.username}</Text>
-                <Text fontSize="md" color="gray.500">{session?.user?.email}</Text>
-            </VStack>
-        </HStack>
+        <Card.Root borderRadius="full">
+            <Card.Body>
+                <HStack gap={4}>
+                    <Avatar.Root size="xl">
+                        <Avatar.Fallback name={session.user.username ? session.user.username : ""} />
+                    </Avatar.Root>
+                    <VStack align="start">
+                        <Text fontSize="2xl" fontWeight="bold">{session?.user?.username}</Text>
+                        <Text fontSize="md" color="gray.500">{session?.user?.email}</Text>
+                    </VStack>
+                </HStack>
+            </Card.Body>
+        </Card.Root>
     );
 };
 
